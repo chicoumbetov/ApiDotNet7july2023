c11e525a58fa4bb56bc0c24773b31487bed8bcaa
chicoumbetov <shyngysmaaan@gmail.com>
2023-07-28 21:02:42 +0200

    post update delete controller superhero created

--- ApiDotNet7july2023/Controllers/SuperHeroController.cs
+++ ApiDotNet7july2023/Controllers/SuperHeroController.cs
@@ -38,7 +38,7 @@ namespace ApiDotNet7july2023.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<List<Models.SuperHero>>> GetSingleHero(int id)
+        public async Task<ActionResult<Models.SuperHero>> GetSingleHero(int id)
         {
             var hero = superHeroes.Find(x => x.Id == id);
             if (hero is null)
@@ -48,20 +48,38 @@ namespace ApiDotNet7july2023.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public async Task<ActionResult<List<Models.SuperHero>>> AddHero(Models.SuperHero hero)
         {
+            superHeroes.Add(hero);
+            return Ok(superHeroes);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public async Task<ActionResult<List<Models.SuperHero>>> UpdateHero(int id, Models.SuperHero heroRequest)
         {
+            var hero = superHeroes.Find(x => x.Id == id);
+            if (hero is null)
+                return NotFound("Sorry, but this hero doesn't exist.");
+
+            hero.FirstName = heroRequest.FirstName;
+            hero.LastName = heroRequest.LastName;
+            hero.Name = heroRequest.Name;
+            hero.Place = heroRequest.Place;
+
+            return Ok(superHeroes);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult<List<Models.SuperHero>>> DeleteHero(int id)
         {
+            var hero = superHeroes.Find(x => x.Id == id);
+            if (hero is null)
+                return NotFound("Sorry, but this hero doesn't exist.");
+
+            superHeroes.Remove(hero);
+            return Ok(superHeroes);
         }
     }
 }
